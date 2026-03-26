6c77c51b43183959565d3147a875dced7d34fc60
Stefan Lendl <git@stfl.dev>
2026-03-26 09:52:54 +0100

    feat: add git-hooks-nix pre-commit hooks for lint and tests
    Adds cachix/git-hooks.nix via flake-parts to run `just lint` and
    `just tests` automatically before each commit. Also excludes .direnv
    and .pre-commit-config.yaml from Biome's file scan.

--- flake.nix
+++ flake.nix
@@ -4,10 +4,13 @@
   inputs = {
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
     flake-parts.url = "https://flakehub.com/f/hercules-ci/flake-parts/0.1";
+    git-hooks-nix.url = "github:cachix/git-hooks.nix";
   };
 
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [inputs.git-hooks-nix.flakeModule];
+
       systems = [
         "x86_64-linux"
         "aarch64-linux"
@@ -15,7 +18,22 @@
         "aarch64-darwin"
       ];
 
-      perSystem = {pkgs, ...}: {
+      perSystem = {pkgs, config, ...}: {
+        pre-commit.settings.hooks = {
+          lint = {
+            enable = true;
+            name = "lint";
+            entry = "just lint";
+            pass_filenames = false;
+          };
+          tests = {
+            enable = true;
+            name = "tests";
+            entry = "just tests";
+            pass_filenames = false;
+          };
+        };
+
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             # Emacs / eask
@@ -46,6 +64,7 @@
             biome
             nodejs
           ];
+          shellHook = config.pre-commit.installationScript;
         };
       };
     };
