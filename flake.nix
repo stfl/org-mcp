45c2563f10b83c84a2dc714c8475838b98de8d04
Stefan Lendl <git@stfl.dev>
2026-03-26 09:34:20 +0100

    fix: remove checkov, reformat flake.nix

--- flake.nix
+++ flake.nix
@@ -6,9 +6,8 @@
     flake-parts.url = "https://flakehub.com/f/hercules-ci/flake-parts/0.1";
   };
 
-  outputs =
-    inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
       systems = [
         "x86_64-linux"
         "aarch64-linux"
@@ -16,40 +15,38 @@
         "aarch64-darwin"
       ];
 
-      perSystem =
-        { pkgs, ... }:
-        {
-          devShells.default = pkgs.mkShell {
-            packages = with pkgs; [
-              # Emacs / eask
-              emacs
-              eask-cli
+      perSystem = {pkgs, ...}: {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            # Emacs / eask
+            emacs
+            eask-cli
 
-              # CI runner
-              act
+            # CI runner
+            act
 
-              # Task runner
-              just
+            # Task runner
+            just
 
-              # Shell checks
-              shellcheck
-              shfmt
+            # Shell checks
+            shellcheck
+            shfmt
 
-              # GitHub Actions checks
-              checkov
-              zizmor
+            # GitHub Actions checks
+            # checkov
+            zizmor
 
-              # Markdown / text
-              mdl
-              prettier
-              textlint
-              textlint-rule-terminology
+            # Markdown / text
+            mdl
+            prettier
+            textlint
+            textlint-rule-terminology
 
-              # JSON / JS
-              biome
-              nodejs
-            ];
-          };
+            # JSON / JS
+            biome
+            nodejs
+          ];
         };
+      };
     };
 }
